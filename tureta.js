e2690fdcc9705888dd71a7d4ae72eee2f307fdc7
iyossias-eshete <53091148+iyossias-eshete@users.noreply.github.com>
2019-09-10 22:03:36 +0300

    Update tureta.js

--- tureta.js
+++ tureta.js
@@ -170,6 +170,29 @@ const requiredMonthlySavings = (amountDifference, yearsToRetirement) => {
     return requiredAnnualSavings(amountDifference, yearsToRetirement) / 12;
 }
 
+
+//test this
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("data.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
+
+//
+
 //module export
 module.exports.displayNumbers = displayNumbers;
 module.exports.pensionCalculator = pensionCalculator;
